f70647a4acbf1f38f15f70618b2ba9d1d1656fed
Isamu Sherman <51054422+isamusherman@users.noreply.github.com>
2024-07-08 16:05:44 +0900

    Added getHumanChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,3 +12,11 @@ function getComputerChoice(){
 for(i = 0; i < 10; i++){
     console.log(getComputerChoice())
 }
+
+function getHumanChoice(){
+    let rawInput = prompt("Enter Rock, Paper, or Scissors")
+    lowerInput = rawInput.toLowerCase()
+    return lowerInput.at(0).toUpperCase() + lowerInput.slice(1)
+}
+
+console.log(getHumanChoice())
