46f468dd68f1256ad2b1df0855375c6b61a19ad8
Isamu Sherman <51054422+isamusherman@users.noreply.github.com>
2024-07-08 16:02:40 +0900

    Added getComputerChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice(){
+    value = Math.random()
+    if (value < .33){
+        return "Rock"
+    }
+    if (value < .67){
+        return "Paper"
+    }
+    return "Scissors"
+}
+
+for(i = 0; i < 10; i++){
+    console.log(getComputerChoice())
+}
