58a11a7d8c3bc22e5021265834b9308eeb061344
Isamu Sherman <51054422+isamusherman@users.noreply.github.com>
2024-07-08 16:29:33 +0900

    Added playRound Function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -19,7 +19,53 @@ function getHumanChoice(){
     return lowerInput.at(0).toUpperCase() + lowerInput.slice(1)
 }
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == computerChoice) {
+        console.log(humanChoice + " vs. " + computerChoice + " It's a draw")
+    }
+    switch(humanChoice) {
+        case "Rock":
+            if(computerChoice == "Scissors"){
+                printHumanWin(humanChoice, computerChoice)
+                humanScore += 1
+            } else{ 
+                printHumanLose(humanChoice, computerChoice)
+                computerScore += 1
+            }
+            break;
+
+        case "Paper":
+            if(computerChoice == "Rock"){
+                printHumanWin(humanChoice, computerChoice)
+                humanScore +=1
+            } else {
+                printHumanLose(humanChoice, computerChoice)
+                computerScore +=1
+            }
+            break;
+        case "Scissors":
+            if(computerChoice == "Paper"){
+                printHumanWin(humanChoice, computerChoice)
+                humanScore += 1
+            } else {
+                printHumanLose(humanChoice, computerChoice)
+                computerScore += 1
+            }
+            break;
+    }
+
+}
+
+function printHumanWin(humanChoice, computerChoice){
+    console.log(humanChoice + " beats " + computerChoice + " You Win!")
+}
+
+function printHumanLose(humanChoice, computerChoice){
+    console.log(computerChoice + " beats " + humanChoice + " You Lose...")
+}
 // console.log(getHumanChoice())
 
 let humanScore = 0
-let ComputerScore = 0
+let computerScore = 0
+
+playRound(getHumanChoice(), getComputerChoice())
